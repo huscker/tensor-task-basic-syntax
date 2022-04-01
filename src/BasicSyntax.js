3542da801c4d688d7e16e1d962b6907de18e4fa4
huscker <egostalker03@mail.ru>
2022-04-01 17:07:02 +0300

    completed task

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -4,5 +4,20 @@ export function romanToInteger(str) {
     Напишите код, преобразующий число из римской записи в арабскую.
     На вход вы получите строку, ответом должно быть число.
      */
+    let alphabet = {
+    	'I': 1,
+    	'V': 5,
+    	'X': 10,
+    	'L': 50,
+    	'C': 100,
+    	'D': 500,
+    	'M': 1000
+    }
+    let prev = 0;
+    for(let i = 0;i<str.length;i++){
+    	val = alphabet[str[i]];
+    	result += prev < val ? val - 2*prev : val;
+    	prev = val;
+    }
     return result;
 }
